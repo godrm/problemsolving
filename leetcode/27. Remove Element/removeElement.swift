8b9c1600b4cdb94d7611a81b10d1e3e4466236bf
Kim Jung <godrm77@gmail.com>
2019-10-31 19:30:46 +0900

    #27 Remove Element 문제 추가

---
+++ leetcode/27. Remove Element/removeElement.swift
@@ -0,0 +1,15 @@
+//
+//  removeElement.swift
+//  leetcode
+//
+//  Created by JK on 2019/10/31.
+//  Copyright © 2019 codesquad. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        return 0
+    }
+}
